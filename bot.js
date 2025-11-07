cec05ee80e7d37fe44aa2878f1be5d98883ca207
seanngo64 <sean_ngo@brown.edu>
2025-11-06 23:24:18 -0500

    openai functionality and .env file

--- bot.js
+++ bot.js
@@ -1,12 +1,19 @@
 // Require the necessary discord.js and openai.js classes
-const { Client, Events, GatewayIntentBits, MessageFlags, REST, Routes, Collection, Emoji } = require('discord.js');
+const { Client, Events, GatewayIntentBits, MessageFlags, REST, Routes, Collection, Emoji, TextChannel } = require('discord.js');
 const config = require('./config.json')
 const fs = require('node:fs');
 const path = require('node:path');
+require('dotenv').config();
+const { OpenAI } = require('openai');
+const { Channel } = require('node:diagnostics_channel');
+const { SlashCommandBuilder } = require('discord.js');
 
 // Create a new client instance
 // Requires GuildMessages and MessageContent for reading
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const clientAI = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY
+});
 
 // // Store commands
 // var commands = new Collection;
@@ -14,7 +21,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 // console.log(client.commands)
 
 // When the client is ready, run this code (only once)
-client.once(Events.ClientReady, (readyClient) => {
+client.once(Events.ClientReady, async (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
 	// const rest = new REST().setToken(config["bot-token"]);
@@ -29,20 +36,65 @@ client.once(Events.ClientReady, (readyClient) => {
 	// }
 });
 
+
+const tools = [
+  {
+    type: "function",
+    name: "get_horoscope",
+    description: "Get today's horoscope for an astrological sign.",
+    parameters: {
+      type: "object",
+      properties: {
+        sign: {
+          type: "string",
+          description: "An astrological sign like Taurus or Aquarius",
+        },
+      },
+      required: ["sign"],
+    },
+  },
+];
+
+function get_horoscope(sign)	{
+	return sign + " This was today's horoscope."
+}
+
+
 // Note that === is a js notation for type checking
-client.on("messageCreate", (msg) => {
+client.on("messageCreate", async (msg) => {
 	if (msg.author.bot) return; // makes sure it can't reply to itself
 	
-	if (msg.content === "ping")	{
+	else if (msg.content === "ping")	{
 		msg.reply("pong")
 	}
 	
-	if (msg.content === "pin me")	{
+	else if (msg.content.includes("pin me"))	{
 		msg.pin()
 	}
 
-	if (msg.content.includes("fire"))	{
+	else if (msg.content.includes("fire"))	{
 		msg.react("🔥")
+	}
+
+	// TODO: implement /summarize for summarizing text threads. 
+	// throws exception? for not threads, or caps length of reading 
+	// and notes it in the response message
+	else if (msg.content.startsWith("!AI")) {
+		const response = await clientAI.responses.create({
+			model: "gpt-5-mini",
+			input: msg.content,
+			reasoning: { effort: "minimal" },
+			text: { verbosity: "low" },
+			// tool_choice: {
+			// 	"type": "custom",
+			// 	"mode": "auto",
+			// 	"tools": [
+			// 		{ type: "function", name: "get-weather", description: "Tells today's weather." }
+			// 	]
+			// },
+			max_output_tokens: 100
+		});
+		msg.reply(response.output_text)
 	}
 })
 
